b479347c2fb32ff75bff2d34223da13f29da172d
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-22 12:13:09 +0530

    Creat Inventory

--- Week1/Algorithms_Data Structures/Exercise-1-InventoryManagement/Inventory.java
+++ Week1/Algorithms_Data Structures/Exercise-1-InventoryManagement/Inventory.java
@@ -1,3 +1,47 @@
+import java.util.HashMap;
+
 public class Inventory {
-    
+
+    private HashMap<Integer, Product> products = new HashMap<>();
+
+    // Add Product
+    public void addProduct(Product product) {
+        products.put(product.getProductId(), product);
+        System.out.println("Product Added Successfully.");
+    }
+
+    // Update Product
+    public void updateProduct(int id, int quantity, double price) {
+
+        if(products.containsKey(id)) {
+
+            Product p = products.get(id);
+
+            p.setQuantity(quantity);
+            p.setPrice(price);
+
+            System.out.println("Product Updated Successfully.");
+        }
+        else {
+            System.out.println("Product Not Found.");
+        }
+    }
+
+    // Delete Product
+    public void deleteProduct(int id) {
+
+        if(products.remove(id) != null)
+            System.out.println("Product Deleted Successfully.");
+        else
+            System.out.println("Product Not Found.");
+    }
+
+    // Display Products
+    public void displayProducts() {
+
+        for(Product p : products.values()) {
+            System.out.println(p);
+        }
+    }
+
 }
