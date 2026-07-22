4780c53bb8eec67899a84cae2d1d1417464799ab
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-22 15:57:23 +0530

    Configuring Authorization Servers and Resource Servers

---
+++ Week5/Configuring Authorization Server/resource-server/src/main/java/com/example/resource_server/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.resource_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(org.springframework.security.config.Customizer.withDefaults());
+
+        return http.build();
+    }
+}
