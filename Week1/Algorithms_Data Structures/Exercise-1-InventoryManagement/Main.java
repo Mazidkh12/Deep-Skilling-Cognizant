e705b2b38f2d8a10bfc79927f15f1c1fa12fe35a
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-22 12:15:46 +0530

    Complete Ex-1 and Successfully run

--- Week1/Algorithms_Data Structures/Exercise-1-InventoryManagement/Main.java
+++ Week1/Algorithms_Data Structures/Exercise-1-InventoryManagement/Main.java
@@ -1,3 +1,30 @@
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Inventory inventory = new Inventory();
+
+        inventory.addProduct(new Product(101, "Laptop", 10, 55000));
+
+        inventory.addProduct(new Product(102, "Mouse", 50, 600));
+
+        inventory.addProduct(new Product(103, "Keyboard", 30, 1200));
+
+        System.out.println("\nInventory:");
+
+        inventory.displayProducts();
+
+        System.out.println("\nUpdating Product...");
+
+        inventory.updateProduct(102, 80, 650);
+
+        inventory.displayProducts();
+
+        System.out.println("\nDeleting Product...");
+
+        inventory.deleteProduct(101);
+
+        inventory.displayProducts();
+
+    }
 }
