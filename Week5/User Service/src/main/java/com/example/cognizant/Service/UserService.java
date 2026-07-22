3854bf23042f207f62a8c02dc7597522c03d79a9
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-22 23:14:59 +0530

    Service Create

---
+++ Week5/User Service/src/main/java/com/example/cognizant/Service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.cognizant.Service;
+
+import com.example.cognizant.Entity.User;
+import com.example.cognizant.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repo;
+
+    public List<User> getAllUsers() {
+        return repo.findAll();
+    }
+
+    public User save(User user) {
+        return repo.save(user);
+    }
+
+    public User getUser(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+}
