60dc4024e8f1ead205242de40f07808852a273d1
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-23 20:38:04 +0530

    Complete React 2

---
+++ Week6/StudentApp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './component/Home'
+import Contact from './component/Contact'
+import About from './component/About'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Contact />
+      <About />
+    </div>
+  )
+}
+
+export default App
---
+++ Week6/StudentApp/src/component/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Welcome to the Contact Page of Student Management Portal
+    </div>
+  )
+}
+
+export default Contact
