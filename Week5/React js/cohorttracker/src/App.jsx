01a4657a7d2f2feae91049c7661376cc608d998f
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-24 12:05:46 +0530

    React Ex-5

---
+++ Week5/React js/cohorttracker/src/App.jsx
@@ -0,0 +1,29 @@
+import CohortDetails from "./CohortDetails";
+import cohorts from "./data";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <h1>Cohorts Details</h1>
+
+            {
+                cohorts.map(cohort => (
+
+                    <CohortDetails
+                        key={cohort.id}
+                        cohort={cohort}
+                    />
+
+                ))
+            }
+
+        </div>
+
+    );
+
+}
+
+export default App;
