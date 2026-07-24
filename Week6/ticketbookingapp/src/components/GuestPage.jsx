5819f0de242cd2de6ac575d866d5fc098cd0b032
Mazidkh12 <mazidkhanss12@gmail.com>
2026-07-24 18:20:59 +0530

    React Exercise 12

---
+++ Week6/ticketbookingapp/src/components/GuestPage.jsx
@@ -0,0 +1,31 @@
+function GuestPage() {
+
+    return (
+
+        <div>
+
+            <h1>Please Sign Up.</h1>
+
+            <h3>Available Flights</h3>
+
+            <ul>
+
+                <li>Mumbai → Delhi</li>
+
+                <li>Chennai → Bangalore</li>
+
+                <li>Hyderabad → Kolkata</li>
+
+            </ul>
+
+            <p>
+                Login to book your ticket.
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default GuestPage;
---
+++ Week6/ticketbookingapp/src/components/UserPage.jsx
@@ -0,0 +1,21 @@
+function UserPage() {
+
+    return (
+
+        <div>
+
+            <h1>Welcome Back</h1>
+
+            <h3>Book Your Flight</h3>
+
+            <button>
+                Book Ticket
+            </button>
+
+        </div>
+
+    );
+
+}
+
+export default UserPage;
